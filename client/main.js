85973b630a53acc085b35e60f9085db10add3d1c
Your Name <karunsingh1993@gmail.com>
2017-10-18 14:07:37 -0400

    created routes for various pages

--- client/main.js
+++ client/main.js
@@ -5,18 +5,18 @@ import { Router, Route, browserHistory } from "react-router";
 
 import Signup from "../imports/ui/Signup";
 import Link from "../imports/ui/Link";
+import Login from "../imports/ui/Login";
+import NotFound from "../imports/ui/NotFound";
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <Signup />
-        <Link />
-      </div>
-    );
-  }
-}
+const routes = (
+  <Router history={browserHistory}>
+    <Route path="/" component={Login} />
+    <Route path="/signup" component={Signup} />
+    <Route path="/links" component={Link} />
+    <Route path="*" component={NotFound} />
+  </Router>
+);
 
 Meteor.startup(() => {
-  ReactDOM.render(<App />, document.getElementById("app"));
+  ReactDOM.render(routes, document.getElementById("app"));
 });
---
+++ imports/ui/NotFound.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class NotFound extends Component {
+  render() {
+    return <p>40404040</p>;
+  }
+}
