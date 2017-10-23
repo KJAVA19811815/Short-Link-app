1f1967390a933af74803d3d65d3813079a7f8737
Your Name <karunsingh1993@gmail.com>
2017-10-23 15:02:32 -0400

    setting up user auth can login and logout

--- imports/ui/Login.js
+++ imports/ui/Login.js
@@ -1,12 +1,42 @@
 import React, { Component } from "react";
 import { Link } from "react-router";
+import { Meteor } from "meteor/meteor";
 
 export default class Login extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      error: ""
+    };
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+    let email = this.refs.email.value.trim();
+    let password = this.refs.password.value.trim();
+
+    Meteor.loginWithPassword({ email }, password, err => {
+      console.log("login callback", err);
+    });
+  }
+
   render() {
     return (
       <div>
-        <h1>LOGIN to start</h1>
-        login form here
+        <h1>SHORT-LINK</h1>
+        {this.state.error}
+
+        <form onSubmit={this.onSubmit.bind(this)}>
+          <input type="text" ref="email" name="email" placeholder="email" />
+          <input
+            type="text"
+            ref="password"
+            name="password"
+            placeholder="password"
+          />
+          <button>LOGIN</button>
+        </form>
         <Link to="/signup">Have an account?</Link>
       </div>
     );
--- imports/ui/Signup.js
+++ imports/ui/Signup.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Link } from "react-router";
+import { Accounts } from "meteor/accounts-base";
 
 export default class Signup extends Component {
   constructor(props) {
@@ -11,10 +12,16 @@ export default class Signup extends Component {
   }
   onSubmit(e) {
     e.preventDefault();
+    let email = this.refs.email.value.trim();
+    let password = this.refs.password.value.trim();
 
-    this.setState({
-      error: "something went wrong"
+    Accounts.createUser({ email, password }, err => {
+      console.log("sign up callback", err);
     });
+
+    // this.setState({
+    //   error: "something went wrong"
+    // });
   }
   render() {
     return (
@@ -22,8 +29,13 @@ export default class Signup extends Component {
         <h1>SIGN UP OR REGISTER</h1>
         {this.state.error}
         <form onSubmit={this.onSubmit.bind(this)}>
-          <input type="text" name="email" placeholder="email" />
-          <input type="text" name="password" placeholder="password" />
+          <input type="text" ref="email" name="email" placeholder="email" />
+          <input
+            type="text"
+            ref="password"
+            name="password"
+            placeholder="password"
+          />
           <button>CREATE ACCOUNT</button>
         </form>
         <Link to="/">LOGIN</Link>
